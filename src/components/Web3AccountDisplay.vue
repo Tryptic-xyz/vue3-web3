3bf33547f4ef4791be788ac18cf236da161c6648
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-20 18:37:41 -0400

    update styling

--- src/components/Web3AccountDisplay.vue
+++ src/components/Web3AccountDisplay.vue
@@ -8,9 +8,15 @@ const wallet = useWalletStore()
 </script>
 
 <template>
-  <div>
-    <div v-if="wallet.address && !wallet.ensName">{{ wallet.prettyAddress }}</div>
-    <div v-if="wallet.ensName">{{ wallet.ensName }}</div>
+  <div class="text-white">
+    <div
+      class="inline-flex items-center px-2 py-1 leading-none text-center text-white bg-black border border-white"
+    >
+      <div class="w-2 h-2 bg-green-300 rounded-full"></div>
+      <div class="ml-2 font-mono text-sm text-white">
+        {{ wallet.ensName || wallet.prettyAddress }}
+      </div>
+    </div>
     <Web3ConnectWalletBtn v-if="!wallet.address && showConnectBtn" />
   </div>
 </template>
