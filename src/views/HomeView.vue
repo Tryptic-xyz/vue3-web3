9578acc46527772446c549be8150e57d3e91f992
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-20 18:37:22 -0400

    remove unused code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,93 +1,44 @@
 <script setup lang="ts">
-import { useWalletStore } from '@/stores/wallet'
-import Web3WalletDisplay from '@/components/Web3AccountDisplay.vue'
-import { useContract } from '@/composables/useContract'
-import abi from '@/abi/oSnipe'
-import { showTxToast } from '@/utils/ToastComponents.jsx'
-import { useUserStore } from '@/stores/user'
-const { user, login, logout } = useUserStore()
-
-const CONTRACT_CONSTANTS = {
-  sniperPriceETH: '0.5',
-  observerPriceETH: '0.03',
-  purveyorPriceETH: '3',
-  sniperID: 0,
-  purveyorID: 1,
-  observerID: 2,
-  committedSniperID: 10,
-  committedPurveyorID: 11,
-  maxSnipersSupply: 488,
-  maxObserversPerCommitted: 10
-}
-
-const wallet = useWalletStore()
-
-const { write, batchRead, onContractInit, txPending } = useContract(
-  '0xAA8E256202067ec9c9c3C9eBA1E5ce6dd273c15C',
-  abi
-)
-
-onContractInit(() => {
-  test()
-})
-
-async function test() {
-  const results = await batchRead([
-    { name: 'alreadyMinted', args: [wallet.address] },
-    {
-      name: 'balanceOf',
-      returnAs: 'hasPurveyorPass',
-      castTo: 'boolean',
-      args: [wallet.address, CONTRACT_CONSTANTS.purveyorID]
-    },
-    {
-      name: 'balanceOf',
-      returnAs: 'hasSniperPass',
-      castTo: 'boolean',
-      args: [wallet.address, CONTRACT_CONSTANTS.sniperID]
-    },
-    {
-      name: 'balanceOf',
-      returnAs: 'hasCommittedSniperPass',
-      castTo: 'boolean',
-      args: [wallet.address, CONTRACT_CONSTANTS.committedSniperID]
-    },
-    {
-      name: 'balanceOf',
-      returnAs: 'hasCommittedPurveyorPass',
-      castTo: 'boolean',
-      args: [wallet.address, CONTRACT_CONSTANTS.committedPurveyorID]
-    },
-    {
-      name: 'balanceOf',
-      returnAs: 'observerBalance',
-      castTo: 'number',
-      args: [wallet.address, CONTRACT_CONSTANTS.observerID]
-    }
-  ])
-}
-
-async function click() {
-  try {
-    const receipt = await write({ name: 'mintObservers', args: [1] }, { value: 5 })
-    console.log(receipt)
-
-    showTxToast(receipt)
-  } catch (error: any) {
-    const e = {
-      error,
-      message: 'There was a problem minting. Maybe you already have the maximum?'
-    }
-    showTxToast(error)
-  }
-}
+import Web3AccountDisplay from '@/components/Web3AccountDisplay.vue'
 </script>
 
 <template>
-  <div>
-    <RouterLink to="about">ABOUT</RouterLink>
-    <Web3WalletDisplay :showConnectBtn="true" />
-    <button v-if="!user.id" :disabled="txPending" @click="() => login()">login</button>
-    <button v-if="user.id" @click="() => logout()">logout</button>
+  <div
+    class="relative isolate overflow-hidden bg-gray-900 w-full flex items-center justify-center"
+    style="height: 100vh"
+  >
+    <div class="py-24 px-6 sm:px-6 sm:py-32 lg:px-8">
+      <div class="mx-auto max-w-2xl text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          Tryptic Has you covered.<br />Enjoy Our Vue3 Web3 Starter.
+        </h2>
+        <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
+          Includes all the basic features to get you connected to the Blockchain, quickly. Like
+          lickety split quickly. Ethers.js is our library of choice.
+        </p>
+        <div class="mt-6 flex items-center justify-center gap-x-6">
+          <Web3AccountDisplay :showConnectBtn="true" />
+        </div>
+      </div>
+    </div>
+    <svg
+      viewBox="0 0 1024 1024"
+      class="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
+      aria-hidden="true"
+    >
+      <circle
+        cx="512"
+        cy="512"
+        r="512"
+        fill="url(#8d958450-c69f-4251-94bc-4e091a323369)"
+        fill-opacity="0.7"
+      />
+      <defs>
+        <radialGradient id="8d958450-c69f-4251-94bc-4e091a323369">
+          <stop stop-color="#7775D6" />
+          <stop offset="1" stop-color="#E935C1" />
+        </radialGradient>
+      </defs>
+    </svg>
   </div>
 </template>
