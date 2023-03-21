e58eab37fa406aa737537230503203280fd16d60
Adrien Hobbs <adrien.hobbs@gmail.com>
2023-03-20 23:20:46 -0400

    only show when provider is ready

--- src/components/Web3AccountDisplay.vue
+++ src/components/Web3AccountDisplay.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+//TODO have wallet init so we can show metamask btn only if no address present after init
 import { useWalletStore } from '@/stores/wallet'
 import Web3ConnectWalletBtn from './Web3ConnectWalletBtn.vue'
+import { useWeb3ProviderStore } from '@/stores/web3Provider'
 
 defineProps({ showConnectBtn: { type: Boolean, default: false } })
 
 const wallet = useWalletStore()
+const provider = useWeb3ProviderStore()
 </script>
 
 <template>
-  <div class="text-white">
+  <div class="text-white" v-if="provider.connected">
     <div
       v-if="wallet.address"
       class="rounded-md inline-flex items-center px-2 py-1 leading-none text-center text-white bg-black border border-white"
